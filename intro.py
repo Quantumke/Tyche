f5a35713b8b313c32c931dd5a3903368bfdec087
Ben <Quantumke@users.noreply.github.com>
2016-05-01 22:50:45 +0300

    tk cc

--- intro.py
+++ intro.py
@@ -8,7 +8,7 @@ class Intro():
 
 	def get_credentials (fetch_data):
 		credentials ={}
-		credentials['api_token']="xoxb-37407461829-eBlHTfhJU6ZoO8vi3Y5u7hnc"
+		credentials['api_token']=""
 		credentials['channel_name']="sandbox"
 		return credentials
 if __name__=='__main__':
